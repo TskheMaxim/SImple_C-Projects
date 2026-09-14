118d526a63cebdbcf8d54b3a9345a4afaef81007
Maxim <makstmainacc@outlook.com>
2026-09-14 11:53:06 +0500

    The Insertion Sort program

---
+++ 004_InsertSort/InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+const int MAX = 10;
+
+void InsertionSort(int arr[], int MAX){
+  for (int i = 1; i<MAX; i++){
+    int temp = arr[i];
+    int j;
+    for (j=i; j>0 && temp<arr[j-1]; j--){
+      arr[j] = arr[j-1];
+    }arr[j] = temp;
+  }
+}
+
+void swap(int a, int b){
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
+}
+
+void printarr(int arr[], int MAX){
+  for (int i=0; i<MAX; i++){
+    cout<<arr[i]<<" ";
+  }
+}
+
+int main(){
+  int arr[MAX] = {3,4,6,9,2,1,8,7,10,5};
+  printarr(arr, MAX);
+  InsertionSort(arr, MAX);
+  cout<<endl;
+  printarr(arr, MAX);
+
+  return 0;
+}
